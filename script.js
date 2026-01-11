c4ecba48e9db656042369bfef4ab1841a94114d6
nailapalijo <nailaqadirpalijo@gmail.com>
2026-01-11 15:54:56 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+AOS.init({ duration: 1000, once: true });
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 70 },
+        "color": { "value": "#0dcaf0" },
+        "opacity": { "value": 0.2 },
+        "size": { "value": 2 },
+        "line_linked": { "enable": true, "distance": 150, "color": "#0dcaf0", "opacity": 0.1 },
+        "move": { "enable": true, "speed": 1 }
+    }
+});
